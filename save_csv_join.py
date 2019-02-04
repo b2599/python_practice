d1844410f298dd98d522561e9c88764991fc01f6
wakuwaku0918 <43364561+wakuwaku0918@users.noreply.github.com>
2019-02-05 00:02:20 +0900

    practice-2019.02.05
    practice-2019.02.05

---
+++ save_csv_join.py
@@ -0,0 +1,8 @@
+print('rank, city, population')
+
+
+print(','.join(['1', 'city1', '24150000']))
+print(','.join(['2', 'city2', '23500000']))
+print(','.join(['3', 'city3', '21516000']))
+print(','.join(['4', 'city4', '14722100']))
+print(','.join(['5', 'city5', '14160467']))
--- urlopen_encoding.py
+++ urlopen_encoding.py
@@ -1,7 +1,7 @@
 import sys
 from urllib.request import urlopen
 
-f = urlopen('http://www.google.com')
+f = urlopen('http://www.hanbit.co.kr/store/books/full_book_list.html')
 
 encoding = f.info().get_content_charset(failobj="utf-8")
 print('encoding:', encoding, file=sys.stderr)
